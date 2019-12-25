3a30f8e5e2eeab2b845c6d325c56e9626b1a4ba0
us <rahmetsaritekin@gmail.com>
2019-12-25 15:56:30 +0300

    add image

--- app/src/main/java/com/saritekin/socialscheduler/CreatePost.java
+++ app/src/main/java/com/saritekin/socialscheduler/CreatePost.java
@@ -2,23 +2,35 @@ package com.saritekin.socialscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TimePicker;
 
+import java.util.Calendar;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class CreatePost extends AppCompatActivity {
 
     private static int RESULT_LOAD_IMAGE = 1;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
+//    @BindView(R.id.btn_date) Button btnDatePicker;
+//    @BindView(R.id.btn_time) Button btnTimePicker;
+    @BindView(R.id.in_date) EditText txtDate;
+    @BindView(R.id.in_time) EditText txtTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -62,8 +74,46 @@ public class CreatePost extends AppCompatActivity {
         }
     }
 
-    @OnClick(R.id.date_set)
-    public void date_setting(){
-        
+    @OnClick(R.id.btn_date)
+    public void choose_date(){
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+    @OnClick(R.id.btn_time)
+    public void choose_time(){
+        // Get Current Time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+
+                        txtTime.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
     }
 }
