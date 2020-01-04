e9bf35886b77acdf258efb197533df45fa73ca5e
us <rahmetsaritekin@gmail.com>
2020-01-04 21:16:56 +0300

    report toast

--- app/src/main/java/com/saritekin/socialscheduler/Settings.java
+++ app/src/main/java/com/saritekin/socialscheduler/Settings.java
@@ -1,8 +1,10 @@
 package com.saritekin.socialscheduler;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,29 +24,40 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         ButterKnife.bind(this);
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        // "login","no" means default string value is "no", so if you didn't set yes after login, it will be no as default
-        if(preferences.getString("login_twitter", "no").equals("yes")){
-            switch_twitter.setChecked(true);
-        } else if (preferences.getString("login_twitter", "no").equals("no")){
-            switch_twitter.setChecked(false);
-        }
-        if(preferences.getString("login_instagram", "no").equals("yes")){
-            switch_instagram.setChecked(true);
-        } else if (preferences.getString("login_instagram", "no").equals("no")){
-            switch_instagram.setChecked(false);
-        }
 
-        if (!switch_twitter.isChecked()){
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString("login_instagram", "no");
-            editor.apply();
-        }
-        if (!switch_twitter.isChecked()){
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString("login_twitter", "no");
-            editor.apply();
-        }
+        switch_twitter.setChecked(preferences.getString("login_twitter", "no").equals("no"));
+        switch_instagram.setChecked(preferences.getString("login_instagram", "no").equals("yes"));
+
+        switch_twitter.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged (CompoundButton buttonView,boolean isChecked){
+                if (isChecked) {
+                    Intent intent = new Intent(Settings.this, TwitterLogin.class);
+                    startActivity(intent);
+                } else {
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("login_twitter", "no");
+                    editor.apply();
+                }
+            }
+
+        });
+
+        switch_instagram.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged (CompoundButton buttonView,boolean isChecked){
+                if (isChecked) {
+                    Intent intent = new Intent(Settings.this, InstagramLogin.class);
+                    startActivity(intent);
+                } else {
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("login_instagram", "no");
+                    editor.apply();
+                }
+            }
+
+        });
     }
 }
